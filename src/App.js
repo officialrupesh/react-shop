d75d932432027b9d979601476dd71a316829f753
rautrupesh1 <iamrupeshraut1@gmail.com>
2022-12-17 11:36:07 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Fetch from './components/Fetch';
 import Products from './components/Products';
 
 function App() {
   return (
    <>
-   {/* <Navbar /> */}
+
    <Products />
-    {/* <Fetch /> */}
-   {/* <Footer /> */}
+   
    </>
   );
 }
